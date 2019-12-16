48ecb8ae84f657c6a4c714ad16a87a05c2778ea4
pukkamustard <pukkamustard@posteo.net>
2019-12-16 08:51:22 +0100

    fix id bug

--- lib/cpub/objects/object.ex
+++ lib/cpub/objects/object.ex
@@ -14,15 +14,18 @@ defmodule CPub.Objects.Object do
   @doc false
   def changeset(object \\ %Object{}, attrs) do
     object
-    |> cast(attrs, [:data])
+    |> cast(attrs, [:id, :data])
     |> autogenerate_id
     |> validate_required([:id, :data])
+    |> unique_constraint(:id)
   end
 
   defp autogenerate_id(changeset) do
     if is_nil(get_change(changeset, :id)) do
       changeset
       |> put_change(:id, CPub.ID.generate)
+    else
+      changeset
     end
   end
 
