46e77f0710f3f4b5a0bcc1d8cf34ccaad61d7e98
rustra <rustra@riseup.net>
2020-03-13 02:17:23 +0100

    Add specs for CPub.ActivityPub context

--- lib/cpub/activity_pub/activity_pub.ex
+++ lib/cpub/activity_pub/activity_pub.ex
@@ -3,23 +3,19 @@ defmodule CPub.ActivityPub do
   ActivityPub context
   """
 
-  alias CPub.{Activity, Object, Repo, User}
+  alias CPub.{Activity, Object, User}
   alias CPub.ActivityPub.Request
   alias CPub.NS.ActivityStreams, as: AS
 
-  @activitystreams RDF.Turtle.read_file!("./priv/vocabs/activitystreams2.ttl")
+  @activity_streams RDF.Turtle.read_file!("./priv/vocabs/activitystreams2.ttl")
   @doc """
   The ActivityStreams 2.0 ontology
   """
-  def activitystreams, do: @activitystreams
-
-  @doc """
-  Gets an actor.
-  """
-  def get_actor!(id), do: Repo.get!(Actor, id)
+  @spec activity_streams :: RDF.Graph.t()
+  def activity_streams, do: @activity_streams
 
   @activity_types SPARQL.execute_query(
-                    @activitystreams,
+                    @activity_streams,
                     SPARQL.query("""
                     select ?activity_type
                     where {
@@ -31,11 +27,18 @@ defmodule CPub.ActivityPub do
   @doc """
   List of all ActivityStreams Activity types
   """
+  @spec activity_types :: [RDF.IRI.t()]
   def activity_types, do: @activity_types
+
+  # @doc """
+  # Gets an actor.
+  # """
+  # def get_actor!(id), do: Repo.get!(Actor, id)
 
   @doc """
   Creates an ActivityPub activity, computes side-effects and runs everything in a transaction.
   """
+  @spec handle_activity(RDF.IRI.t(), RDF.Graph.t(), User.t()) :: Request.commit_result()
   def handle_activity(%RDF.IRI{} = activity_id, %RDF.Graph{} = data, %User{} = user) do
     # create a new pipeline
     Request.new(activity_id, data, user)
@@ -62,6 +65,7 @@ defmodule CPub.ActivityPub do
     |> Request.commit()
   end
 
+  @spec ensure_correct_actor(Request.t()) :: Request.t()
   defp ensure_correct_actor(%Request{} = request) do
     case request.activity[AS.actor()] do
       nil ->
@@ -82,6 +86,7 @@ defmodule CPub.ActivityPub do
     end
   end
 
+  @spec insert_activity(Request.t()) :: Request.t()
   defp insert_activity(%Request{} = request) do
     activity =
       request.activity
@@ -91,6 +96,7 @@ defmodule CPub.ActivityPub do
     Request.insert(request, :activity, activity)
   end
 
+  @spec set_object_id(Request.t()) :: Request.t()
   defp set_object_id(%Request{} = request) do
     if RDF.iri(AS.Create) in request.activity[RDF.type()] do
       activity =
@@ -106,6 +112,7 @@ defmodule CPub.ActivityPub do
   end
 
   # Creates an object if it is a Create activity
+  @spec create_object(Request.t()) :: Request.t()
   defp create_object(request) do
     if RDF.iri(AS.Create) in request.activity[RDF.type()] do
       case request.data[request.id][AS.object()] do
--- lib/cpub/public.ex
+++ lib/cpub/public.ex
@@ -4,20 +4,20 @@ defmodule CPub.Public do
   """
   import Ecto.Query, only: [from: 2]
 
+  alias CPub.{Activity, Repo}
   alias CPub.NS.ActivityStreams, as: AS
 
   @doc """
   Returns all public activities.
   """
+  @spec get_public :: [Activity.t()]
   def get_public do
-    public_collection = RDF.IRI.new(AS.Public)
+    public_collection = RDF.iri(AS.Public)
 
-    public_query =
-      from a in CPub.Activity,
-        where: ^public_collection in a.recipients
+    public_query = from a in Activity, where: ^public_collection in a.recipients
 
     public_query
-    |> CPub.Repo.all()
-    |> CPub.Repo.preload(:object)
+    |> Repo.all()
+    |> Repo.preload(:object)
   end
 end
