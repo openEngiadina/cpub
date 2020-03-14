38625de6aba04a2618485617dae5f129207efd78
rustra <rustra@riseup.net>
2020-03-14 17:02:23 +0100

    Wrap doctor script to mix task

--- mix.exs
+++ mix.exs
@@ -93,7 +93,8 @@ defmodule CPub.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      doctor: "cmd script/doctor"
     ]
   end
 end
