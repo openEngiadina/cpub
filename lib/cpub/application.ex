7fbf6615965bb7e658b6c45ce5958178b7f76a13
pukkamustard <pukkamustard@posteo.net>
2020-11-16 21:22:53 +0100

    CPub.Application: log Application version on startup

--- lib/cpub/application.ex
+++ lib/cpub/application.ex
@@ -5,22 +5,29 @@ defmodule CPub.Application do
 
   use Application
 
+  require Logger
+
   alias CPub.Web.Endpoint
 
   @spec start(Application.start_type(), term) ::
           {:ok, pid} | {:ok, pid, Application.state()} | {:error, term}
   def start(_type, _args) do
     children = [
+      {Task, &log_application_info/0},
       CPub.Repo,
       CPub.Web.Endpoint,
       CPub.Database
-
-      # Start the PubSub system
-      # {Phoenix.PubSub, name: CPub.PubSub},
     ]
 
     opts = [strategy: :one_for_one, name: CPub.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp log_application_info() do
+    with {:ok, version} <- CPub.MixProject.project() |> Keyword.fetch(:version),
+         {:ok, source_url} <- CPub.MixProject.project() |> Keyword.fetch(:source_url) do
+      Logger.info("Starting CPub (" <> version <> "; " <> source_url <> ")")
+    end
   end
 
   @spec config_change(keyword, keyword, [atom]) :: :ok
