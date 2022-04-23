3bea3f4c25239c2b9a8ad78a7a5ee6ee8ddc0e3f
pukkamustard <pukkamustard@posteo.net>
2022-04-23 13:15:53 +0200

    guix.scm: init

---
+++ guix.scm
@@ -0,0 +1,16 @@
+(use-modules
+ (guix packages)
+ (guix build-system gnu)
+ (gnu packages elixir))
+
+(package
+  (name "cpub")
+  (version "0")
+  (home-page #f)
+  (source #f)
+  (build-system gnu-build-system)
+  (native-inputs
+   (list elixir))
+  (synopsis #f)
+  (description #f)
+  (license #f))
